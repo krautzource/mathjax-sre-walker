617b7a897f0fda14dfbc6f4277d9e643de5828c2
Peter Krautzberger <p.krautzberger@gmail.com>
2018-06-26 11:15:37 +0200

    add license and boilerplate * add LICENSE * chromify: add boilerplate * add AUTHORS.txt with contributors * add README.md Resolves #1

--- chromify.js
+++ chromify.js
@@ -1,3 +1,14 @@
+// Copyright 2018 by the authors (see AUTHORS.txt)
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 const chromify = {};
 
@@ -143,7 +154,7 @@ class node {
     this.parent = null;
     this.children = [];
   }
-  
+
 }
 
 
@@ -159,13 +170,13 @@ class tree {
       this.active = this.active.parent;
     }
   }
-  
+
   down() {
     if (this.active.children.length) {
       this.active = this.active.children[0];
     }
   }
-  
+
   left() {
     if (this.active.parent) {
       let index = this.active.parent.children.indexOf(this.active);
@@ -174,7 +185,7 @@ class tree {
       }
     }
   }
-  
+
   right() {
     if (this.active.parent) {
       let index = this.active.parent.children.indexOf(this.active);
@@ -183,7 +194,7 @@ class tree {
       }
     }
   }
-  
+
 }
 
 
