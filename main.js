0bd1cf27b2e822196b1a4d4dc56f8deeda46fc57
Peter Krautzberger <p.krautzberger@gmail.com>
2018-10-01 21:42:24 +0200

    [main.js] fix import path for github.io

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import {rewriteNode} from '/rewritenode.mjs';
-import {attachNavigator}  from '/navigator.mjs';
+import {rewriteNode} from './rewritenode.mjs';
+import {attachNavigator}  from './navigator.mjs';
 
 document.querySelectorAll('[data-semantic-structure]').forEach((node, index)=>{
     rewriteNode(node, index);
